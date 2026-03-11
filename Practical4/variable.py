5d819a07b7beffa24d687e4f4ccd23df0a38dda3
Huang Chongyan <chongyan.25@intl.zju.edu.cn>
2026-03-11 15:54:15 +0800

    add variable.py

---
+++ Practical4/variable.py
@@ -0,0 +1,18 @@
+a=5.08
+b=5.33
+c=5.55
+d=b-a
+e=c-b
+print(d,e)
+#d is larger than e
+#Population growth in Scotland is decelerating.
+
+X = True
+Y = False
+W = X or Y
+#truth table:
+# X | Y | W
+# T | F | T
+# F | T | T
+# F | F | F
+# T | T | T
