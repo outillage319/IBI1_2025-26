e32e9db8bf1400f67b34c090cb2f8279c03f258d
Huang Chongyan <chongyan.25@intl.zju.edu.cn>
2026-03-13 09:34:19 +0800

    Refactor creatinine clearance calculation code

--- Practical4/creatine_clearance.py
+++ Practical4/creatine_clearance.py
@@ -1,11 +1,11 @@
 age = int (input("Please enter the age of the student in years: "))
 weight = int (input("Please enter the weight of the student in kg: "))
 Cr = float (input("Please enter your serum creatinine level (μmol/L): "))
-gender = input("Please enter your gender (M/F): ")
+gender = input("Please enter your gender (M/F): ") #get basic data
 if gender == "M":
     k = 1  
 else:
-    k = 0.85
+    k = 0.85 #determine coefficient related to gender
 if age >= 100:
     print("The age of the student should be less than 100 years.")
 elif weight <= 20 or weight >= 80:
@@ -13,9 +13,8 @@ elif weight <= 20 or weight >= 80:
 elif Cr <= 0 or Cr >= 100:
     print("The serum creatinine level should be between 0 and 100 μmol/L.")
 elif gender != "M" and gender != "F":
-    print("The gender of the student should be either M or F.")
+    print("The gender of the student should be either M or F.") #ensure whether basic data are within normal range
 else:
-    CrCl = ((140 - age) * weight * k) / (Cr * 72)
+    CrCl = ((140 - age) * weight * k) / (Cr * 72) #bring data into the formula
     print("The creatinine clearance of the student is:", CrCl)
-    
     
