0fbba033efeda5d08d4f5e2f8caaeb0224651d1e
Huang Chongyan <chongyan.25@intl.zju.edu.cn>
2026-03-13 09:23:42 +0800

    Refactor infection.py for clarity and formatting

--- Practical4/infection.py
+++ Practical4/infection.py
@@ -1,10 +1,10 @@
-x = 5
-y = 0.4
-i =1
+x = 5 #initial infected students
+y = 0.4 #infection rate
+i =1 #day number
 while x <= 91:
-    i += 1
-    z = x * y
-    x += z
-    print("The number of infected students in " + str(i) + " days is:", z)
+    i += 1 #accumulated day number
+    z = x * y #infected student number in the day
+    x += z #accumulated infected student number
+    print("The number of infected students in day " + str(i) + " is:", z)
 else:
     print("The whole days are:", i)
