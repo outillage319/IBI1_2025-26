3cb186fa52364ed086f0d8a09bbbe4b97b81493b
Huang Chongyan <chongyan.25@intl.zju.edu.cn>
2026-03-16 00:57:43 +0800

    Refactor infection.py with comments and formatting

--- Practical4/infection.py
+++ Practical4/infection.py
@@ -1,3 +1,8 @@
+#x is initial infected students and y is infection rate
+#i counts days
+#while loop for the max student number 91
+#z is infected students every day
+#when breaking the loop, output the whole days
 x = 5 #initial infected students
 y = 0.4 #infection rate
 i =1 #day number
